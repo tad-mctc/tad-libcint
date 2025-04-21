b58a3ee1935bd118b41722593faf83c8a5a664c9
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2025-04-21 22:02:04 +0200

    Update integrals

--- assets/wheels.fish
+++ assets/wheels.fish
@@ -28,14 +28,14 @@ function handle_exit
 end
 trap handle_exit SIGINT SIGTERM
 
-set -l VERSIONS "38" "39" "310" "311" "312"
+set -l VERSIONS "312"
 
 for VERSION in $VERSIONS
     if ! conda env list | grep "wheel-$VERSION" >/dev/null 2>/dev/null
         echo "Creating wheel-$VERSION"
 
         set -l python_version (string replace -r '^(.)(.*)$' '$1.$2' $VERSION)
-        mamba create -n "wheel-$VERSION" --yes "python=$python_version" auditwheel "c-compiler=1.5.2" "fortran-compiler=1.5.2" cmake "libgcc<14" "libgfortran<14" "libstdcxx<14" meson numpy pip python-build pkgconfig patchelf "sysroot_linux-64=2.12" unzip wheel
+        mamba create -n "wheel-$VERSION" --yes "python=$python_version" auditwheel "c-compiler=1.5.2" "cxx-compiler=1.5.2" "fortran-compiler=1.5.2" cmake "libgcc<14" "libgfortran<14" "libstdcxx<14" meson numpy pip python-build pkgconfig patchelf "sysroot_linux-64=2.12" unzip wheel
     end
 
     if test -f "wheels/tad_libcint-0.1.1-cp$VERSION-cp$VERSION-manylinux_2_12_x86_64.manylinux2010_x86_64.whl"
--- test/test_libs.py
+++ test/test_libs.py
@@ -27,13 +27,11 @@ def test_load() -> None:
     cgto = tad_libcint.CGTO
     cint = tad_libcint.CINT
 
-    # test loading the functions
+    # test loading integrals
     gto1 = getattr(cgto, "GTOval_cart")
     gto2 = getattr(cgto, "GTOval_ip_cart")
     cint1 = getattr(cint, "int1e_ovlp_sph")
-    cint2 = getattr(cint, "int1e_rrkin_sph")
 
     assert gto1.__name__ == "GTOval_cart"
     assert gto2.__name__ == "GTOval_ip_cart"
     assert cint1.__name__ == "int1e_ovlp_sph"
-    assert cint2.__name__ == "int1e_rrkin_sph"
